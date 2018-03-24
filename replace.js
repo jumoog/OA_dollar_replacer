df2971aefac40094c637527423fb56c26f90116e
Kilian von Pflugk <github@jumoog.io>
2018-03-24 11:41:19 +0100

    log replaced dollar parameters

--- replace.js
+++ replace.js
@@ -24,7 +24,11 @@ function rewrite(name, value) {
   let found;
   let stop = false;
   result.elements[0].elements[1].elements.forEach(k => {
-      if (k.attributes.Name === name) {
+    if (k.attributes.Name === name) {
+      fs.appendFile(`${process.argv[2]}_result.csv`, `${name};dollarParameters;${value}\n`, function(err) {
+        // abort if we can't write the file
+        if (err) throw err;
+      });
       k.elements[0].elements.forEach(l => {
         // look for dollarParameters
         if (l.attributes.name === "dollarParameters") {
@@ -38,7 +42,7 @@ function rewrite(name, value) {
                   stop = true;
                 }
                 // look for Dollar and set found to true
-                if (j.attributes.name === "Dollar" && j.elements[0].text === "$DP" ) {
+                if (j.attributes.name === "Dollar" && j.elements[0].text === "$DP") {
                   found = true;
                 }
               }
@@ -54,19 +58,19 @@ function writexml() {
   let xml = convert.js2xml(result, options);
 
   fs.writeFile(process.argv[2], xml, (err, data) => {
-      if (err) console.log(err);
-      console.log("successfully written our update to the Panel");
+    if (err) console.log(err);
+    console.log("successfully written our update to the Panel");
   });
 }
 
 csv({
     noheader: true,
     delimiter: "auto"
-})
-    .fromFile(process.argv[2] + '.csv')
-    .on('end_parsed', (jsonObj) => {
-        json = jsonObj;
-    })
-    .on('done', (error) => {
-        processArray(json);
-    });
+  })
+  .fromFile(process.argv[2] + '.csv')
+  .on('end_parsed', (jsonObj) => {
+    json = jsonObj;
+  })
+  .on('done', (error) => {
+    processArray(json);
+  });
